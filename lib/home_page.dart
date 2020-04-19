787e51a420a7569e29bacf1d03b0a881078e2009
iqrafatimame <candy.iqo@gmail.com>
2020-04-19 15:47:30 -0700

    https://github.com/iqrafatimame/COVID-19-flutter-app.git

--- lib/home_page.dart
+++ lib/home_page.dart
@@ -1,7 +1,8 @@
+import 'dart:async';
+ import 'dart:convert';
 import 'package:flutter/material.dart';
 import 'package:http/http.dart' as http;
-import 'dart:async';
-import 'dart:convert';
+
 
 class First extends StatefulWidget{ 
    @override 
@@ -10,53 +11,129 @@ class First extends StatefulWidget{
 
  class FirstState extends State<First>  {
    List countryData;
-   // to get data
-  Future getData () async{ 
-    http.Response response = await http.get("https://corona.lmao.ninja/countries?sort=country");
-
-    setState((){
+   Future getData() async{ 
+     http.Response response = await http.get("https://corona.lmao.ninja/v2/countries");
+      setState((){
         countryData = json.decode(response.body);
         print(countryData);
     });
-  }
-
-  @override
-  void initState(){
+     debugPrint(response.body);
+   }
+   @override
+  void initState() {
     super.initState();
-    getData();   // getting data from corona API
+    getData();
   }
 
    @override 
    Widget build(BuildContext context){
      return Scaffold( 
-        body: ListView.builder(
-          itemCount: countryData == null ? 0 : countryData.length,
-          itemBuilder: (BuildContext context, int index){
-            return  Card(
-              color: Colors.white,
-              shape: RoundedRectangleBorder(
-              borderRadius: BorderRadius.circular(25.0),
-              ),
-              child: ListTile(
-                title: Text("${countryData[index]["country"]}",
-                style: TextStyle( 
-                    color: Colors.black,
-                    fontWeight: FontWeight.bold,
-                    fontSize: 19,
+        body: 
+            ListView.builder(
+              itemCount: countryData == null ? 0 : countryData.length,
+              itemBuilder: (BuildContext context, int index){
+                return  Card(
+                  color: Colors.white,
+                  shape: RoundedRectangleBorder(
+                  borderRadius: BorderRadius.only(
+                      bottomRight: Radius.circular(15),
+                   ),
                   ),
-                ),
-                leading: Icon(Icons.flag,
-                color: Colors.blue),
-                subtitle: Text("Total Cases: ${countryData[index]["cases"]}\n Deaths: ${countryData[index]["deaths"]} \n Recovered: ${countryData[index]["recovered"]}", 
-                style: TextStyle( 
-                  fontSize: 12
-                )),
-                isThreeLine: true,
-              ),
-              shadowColor: Colors.grey,
-            );
-          },
-        ),
-     );
-   }
- }
+                  child: Container( 
+                    child: Column(
+                      children: <Widget>[
+                        Container(
+                          alignment: Alignment.centerLeft,
+                          height: 60.0,
+                          child: Padding(
+                            padding: const EdgeInsets.only( 
+                              top: 20.0,
+                              left: 20.0,
+                            ),
+                            child: Text('${countryData[index]["country"]}',
+                            style: TextStyle( 
+                                color: Colors.grey[700],
+                                fontSize: 20.0,
+                                fontWeight: FontWeight.bold,
+                               ),
+                             ),
+                          ),
+                        ),
+                        Padding(
+                          padding: const EdgeInsets.only( 
+                            bottom: 20.0,
+                            top: 20.0,
+                          ),
+                          child: Center(
+                            child: Row(
+                              mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+                              children: [
+                              Column(
+                                children: <Widget>[ 
+                                      Text('Total Cases',
+                                      style: TextStyle( 
+                                        color:Colors.black54,
+                                       ),
+                                      ),
+                                      SizedBox(height: 15.0),
+                                      Text('${countryData[index]["cases"]}',
+                                      style: TextStyle( 
+                                        color: Colors.blue,
+                                        fontSize: 35.0,
+                                        fontWeight: FontWeight.bold,
+                                      ),
+                                     ),
+                                ],
+                              ),
+                               Column(
+                                children: <Widget>[ 
+                                      Text('Deaths',
+                                      style: TextStyle( 
+                                        color:Colors.black54,
+                                        ),
+                                       ),
+                                      SizedBox(height: 15.0),
+                                      Text('${countryData[index]["deaths"]} ',
+                                      style: TextStyle( 
+                                        color: Colors.red,
+                                        fontSize: 35.0,
+                                        fontWeight: FontWeight.bold,
+                                      ),
+                                    ),
+                                ],
+                              ),
+                               Column(
+                                children: <Widget>[ 
+                                      Text('Recovered',
+                                      style: TextStyle( 
+                                        color:Colors.black54,
+                                       ),
+                                      ),
+                                      SizedBox(height: 15.0),
+                                      Text('${countryData[index]["recovered"]}',
+                                      style: TextStyle( 
+                                        color: Colors.green,
+                                        fontSize: 35.0,
+                                        fontWeight: FontWeight.bold,
+                                      ),
+                                    ),
+                                ],
+                                ),
+                             ],
+                            ),
+                          ),
+                        ),
+                        Container( 
+                          height: 20.0,
+                        ),
+                      ],
+                    ),
+                    
+                  ),
+                  shadowColor: Colors.grey,
+                );
+              },
+            ),
+         );
+       }
+    }
--- lib/main.dart
+++ lib/main.dart
@@ -33,19 +33,22 @@ class MyTabsState extends State<MyTabs> with SingleTickerProviderStateMixin {
   Widget build(BuildContext context){ 
     return new Scaffold( 
       appBar: new AppBar( 
-         title: new Text("COVID'19"),
+         title: new Text("COVID-19"),
          backgroundColor: Colors.green,
         bottom: new TabBar( 
           controller: controller,
             tabs: <Tab>[ 
               new Tab( 
                 icon: new Icon(Icons.home),
+                text: 'Home',
               ),
                new Tab( 
                 icon: new Icon(Icons.grid_on),
+                text: 'Guides',
               ),
                new Tab( 
-                icon: new Icon(Icons.help),
+                icon: new Icon(Icons.graphic_eq),
+                text: 'graph',
               ),
             ],
          ),
