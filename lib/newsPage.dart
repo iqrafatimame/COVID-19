178b0ccf75c6caf57efce2fea559caee0aacef28
iqrafatimame <candy.iqo@gmail.com>
2020-04-26 14:48:27 -0700

    👉 Added News Section

---
+++ lib/newsPage.dart
@@ -0,0 +1,11 @@
+import 'package:corona_virus/viewModels/newsList.dart';
+import 'package:flutter/material.dart';
+class News extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      body: NewsList(),
+      
+    );
+  }
+}
